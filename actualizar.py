4175b3428bd0d3738eca6c48bcbe8e5c7582a929
Ignacio Juan Martín Benedetti <tranceway@gmail.com>
2014-05-12 15:40:36 -0300

    Test y refactor parcial

---
+++ actualizar.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+
+import os
+import shutil
+import re
+
+import apt
+import apt.progress
+
+HUAYRA_VERSION_LIST = {
+    '1.0': 'brisa',
+    '2.0': 'pampero'
+}
+
+
+class HuayraUpdate(object):
+    rutas = {
+        'source.list': '/etc/apt/sources.list',
+        'source.list_backup': '/etc/apt/sources.list.%s',
+        'huayra_repo_url': 'http://repo.huayra.conectarigualdad.gob.ar/huayra',
+        'huayra_version': '/etc/huayra_version'
+    }
+
+    def __init__(self, *args, **kwargs):
+        if kwargs.get('rutas'):
+            self.rutas = kwargs['rutas']
+
+        self._source_list_backup = self.rutas['source.list_backup'] % (HUAYRA_VERSION_LIST[self.version_actual])
+        self._version_destino = '2.0'
+
+
+    @property
+    def version_actual(self):
+        if not hasattr(self, '_version_actual'):
+            try:
+                fd = open(self.rutas['huayra_version'], 'r')
+
+            except IOError:
+                self._version_actual = '1.0'
+
+            else:
+                with fd:
+                    version = fd.readline()
+                    m = re.match(r'(\d+\.\d*)', version)
+
+                    self._version_actual = m.group(1)
+
+        return self._version_actual
+
+    def resguardad_repos(self):
+        if not os.path.isfile(self._source_list_backup):
+            shutil.copy2(self.rutas['source.list'], self._source_list_backup)
+
+    def modificar_repos(self):
+        with open(SOURCE_LIST, 'r') as fd:
+            renglones = fd.readlines()
+
+        for index, data in enumerate(renglones):
+            if HUAYRA_VERSION_LIST['1.0'] in data and HUAYRA_REPO in data:
+                renglones[index] = data.replace(HUAYRA_VERSION_LIST['1.0'], HUAYRA_VERSION_LIST['2.0'])
+
+        with open(SOURCE_LIST, 'w') as fd:
+            fd.write('\n'.join(renglones))
+
+
+
+def actualizar_huayra():
+    if not es_brisa():
+        print('Ya estás usando la última versión disponible de Huayra!')
+
+    else:
+        if not os.path.isfile(SOURCE_LIST_BACKUP):
+            shutil.copy2(SOURCE_LIST, SOURCE_LIST_BACKUP)
+
+        actualizar_distro()
+        print 'Brisa Update'
+
+        cambiar_repo()
+        print('cambia repo')
+
+        actualizar_distro()
+        print('listo')
+
+
+def actualizar_distro():
+    try:
+        cache = apt.Cache()
+        cache.update(apt.progress.text.AcquireProgress())
+        cache.open(None)
+
+        cache.upgrade(True)
+
+        cache.commit(apt.progress.text.AcquireProgress(),
+                     apt.progress.base.InstallProgress())
+    except Exception, e:
+        print e
+        print dir(e)
+        raise ''
+
+
+if __name__ == '__main__':
+    print('+----')
+    print('| Bienvenido al actualizador de Huayra!')
+    print('+----\n')
+
+    if not os.access(SOURCE_LIST, os.W_OK):
+        print(' El actualizador debe ser ejecutado como root.')
+        print(' Ej.: $ sudo huayra-update\n')
+
+    else:
+        respuesta = raw_input('¿Desea continuar? (S)í/(N)o: ')
+        if respuesta == 's':
+            print('')
+            actualizar_huayra()
---
+++ test_actualizar.py
@@ -0,0 +1,92 @@
+import unittest
+import os
+
+from actualizar import HuayraUpdate
+
+RUTAS_TESTING = {
+    'source.list': './tests/sources.list',
+    'source.list_backup': './tests/sources.list.%s',
+    'huayra_repo_url': 'http://repo.huayra.conectarigualdad.gob.ar/huayra',
+    'huayra_version': './tests/huayra_version'
+}
+
+HUAYRA_1_REPOS = """
+#
+
+# deb cdrom:[Debian GNU/Linux 7.0.0 _Wheezy_ - Official Snapshot i386 LIVE/INSTALL Binary 20130614-19:09]/ wheezy contrib main non-free
+
+## Repositorio oficial de Debian GNU/Linux
+deb http://ftp.debian.org/debian/ wheezy main contrib non-free
+
+## Repositorio de Huayra GNU/Linux
+deb http://repo.huayra.conectarigualdad.gob.ar/huayra/ brisa main contrib non-free
+
+## Repositorio de Huayra GNU/Linux (updates)
+deb http://repo.huayra.conectarigualdad.gob.ar/huayra/ brisa-updates main contrib non-free
+
+## Repositorio de Mate Desktop
+deb http://repo.huayra.conectarigualdad.gob.ar/huayra/ mate-brisa main
+
+deb http://security.debian.org/ wheezy/updates main contrib non-free
+deb-src http://security.debian.org/ wheezy/updates main contrib non-free
+"""
+
+
+class Test(unittest.TestCase):
+    def test_reescribir_rutas(self):
+        a = HuayraUpdate()
+        b = HuayraUpdate(rutas=RUTAS_TESTING)
+
+        self.assertNotEqual(a.rutas, b.rutas)
+
+    def test_version_brisa(self):
+        open(RUTAS_TESTING['huayra_version'], 'w').write('')
+        os.unlink(RUTAS_TESTING['huayra_version'])
+
+        a = HuayraUpdate(rutas=RUTAS_TESTING)
+        self.assertEqual(a.version_actual, '1.0')
+
+    def test_version_pampero(self):
+        with open(RUTAS_TESTING['huayra_version'], 'w') as fd:
+            fd.write('2.0RC1')
+
+        a = HuayraUpdate(rutas=RUTAS_TESTING)
+        self.assertEqual(a.version_actual, '2.0')
+
+    def test_resguardar(self):
+        open(RUTAS_TESTING['huayra_version'], 'w').write('')
+        os.unlink(RUTAS_TESTING['huayra_version'])
+
+        with open(RUTAS_TESTING['source.list'], 'w') as fd:
+            fd.write(HUAYRA_1_REPOS)
+
+        a = HuayraUpdate(rutas=RUTAS_TESTING)
+        a.resguardad_repos()
+
+        with open(RUTAS_TESTING['source.list'], 'r') as fd:
+            archivo_original = fd.read()
+
+        with open(RUTAS_TESTING['source.list_backup'] % a.version_actual, 'r') as fd:
+            archivo_resguardo = fd.read()
+
+        self.assertEqual(archivo_original, archivo_resguardo)
+
+        ### ------
+
+        with open(RUTAS_TESTING['source.list'], 'w') as fd:
+            fd.write('TEST')
+
+        a = HuayraUpdate(rutas=RUTAS_TESTING)
+        a.resguardad_repos()
+
+        with open(RUTAS_TESTING['source.list'], 'r') as fd:
+            archivo_original = fd.read()
+
+        with open(RUTAS_TESTING['source.list_backup'] % a.version_actual, 'r') as fd:
+            archivo_resguardo = fd.read()
+
+        self.assertNotEqual(archivo_original, archivo_resguardo)
+
+
+if __name__ == '__main__':
+    unittest.main()
